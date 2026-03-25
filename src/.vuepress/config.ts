92bd81e920ba655900c446ed576175bfed0f7966
中国心\84058 <840589040@qq.com>
2026-03-25 09:04:35 +0800

    更新文案

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,7 +7,9 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "博客演示",
   description: "vuepress-theme-hope 的博客演示",
-
+  // head: [
+  //   ['link', { rel: 'icon', href: 'https://s3.bmp.ovh/imgs/2023/02/15/16aa54f3ee84602e.webp' }]
+  // ],
   theme,
 
   // 和 PWA 一起启用
